f7650ee8860474803fab5b096a64db405dca628e
Wiktoria Luszczek <tori@LAPTOP-9BFLS748>
2023-03-20 17:27:26 +0100

    jss-conditions

--- js-conditions-and-booleans/js-conditions-and-booleans_teenager-check/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_teenager-check/js/index.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('[data-js="age-input"]');
 const button = document.querySelector('[data-js="age-button"]');
-const output = document.querySelector('[data-js="age-output"]');
+let output = document.querySelector('[data-js="age-output"]');
 
 button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
@@ -10,4 +10,11 @@ button.addEventListener("click", () => {
   // Exercise:
   // Use conditions and the logical AND operator to write
   // "You are a teen." or "You are not a teen." into the output.
+
+  if (age > 12 && age < 20) {
+    output.innerHTML = "You are a teen!";
+  } else {
+    output.innerHTML = "You are not a teen!";
+  }
 });
+//I had to change the output into LET instead of CONST to be able to change the inner HTML and be able to insert the text into the output instead of doing console.log
