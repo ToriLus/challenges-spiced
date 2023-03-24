afcc13281f30cf8d218b8963f20758b25c7b7a16
Wiktoria Luszczek <tori@LAPTOP-9BFLS748>
2023-03-24 11:36:10 +0100

    toast message

--- js-createelement_toast-messages/js/index.js
+++ js-createelement_toast-messages/js/index.js
@@ -6,8 +6,19 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+
+  console.log("it works!");
+  let newToast = document.createElement("li");
+  toastContainer.append(newToast);
+  newToast.innerHTML = `
+    <li class="toast-container__message">This is a toast message</li>
+  `;
+
+  console.log(newToast);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  console.log("i hope it works");
+  toastContainer.innerHTML = "";
 });
