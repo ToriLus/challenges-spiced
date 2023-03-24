99f889bd717554808764a693c957d0e61b95ae4a
Wiktoria Luszczek <tori@LAPTOP-9BFLS748>
2023-03-24 12:09:15 +0100

    added the social post

--- js-createelement_social-media-post/js/index.js
+++ js-createelement_social-media-post/js/index.js
@@ -10,3 +10,42 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+// const post = document.createElement("article");
+
+// const paragraph = document.createElement("p");
+
+// paragraph.classList.add("post__content");
+
+// paragraph.textContent = "lorem  ipsum something something";
+
+// post.append(paragraph);
+
+// console.log(post);
+
+// const button = document.createElement('button');
+
+// button.setAttribute('type', 'button');
+
+// button.innerText = 'click me';
+
+// //append it to the body
+
+// document.body.append(post);
+
+let newElement = document.createElement("article");
+newElement.innerHTML = `
+<article class="post">
+  <p class="post__content">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+  </p>
+  <footer class="post__footer">
+    <span class="post__username">@username</span>
+    <button type="button" class="post__button" data-js="like-button">
+      ♥ Like
+    </button>
+    </footer>`;
+
+document.body.append(newElement);
+
+button.addEventListener("click", handleLikeButtonClick);
