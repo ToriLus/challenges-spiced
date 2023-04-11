eec235179ee8df48b1b1c1427232bc00c0c5ce24
Wiktoria Luszczek <tori@LAPTOP-9BFLS748>
2023-04-11 17:27:48 +0200

    react challenges done

--- react-basics_hello-world/src/App.js
+++ react-basics_hello-world/src/App.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return (
+    <div>
+      Hello! Is it me you're looking for? <h1>I can see it in your eyes.</h1>
+      <h2>I can see it in your heart</h2>
+      <h3>I don't actually know the words of this song</h3>
+    </div>
+  );
 }
