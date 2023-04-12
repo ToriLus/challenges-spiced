3a2a26c5724006c91f827497c87dda8c87dc41d3
Wiktoria Luszczek <tori@LAPTOP-9BFLS748>
2023-04-12 11:57:27 +0200

    adding react stuff

--- react/react-props_greeting/src/App.js
+++ react/react-props_greeting/src/App.js
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <h1>
+      Hello
+      <Greeting name="Amy" />
+    </h1>
+  );
+}
+
+function Greeting({ name }) {
+  return <h1> {name} </h1>;
 }
