cc1ab20527fd30e1bc589f5d29ce86fb18019e72
Tori Luszczek <wuszczek@gmail.com>
2023-05-04 10:24:42 +0200

    added challenges and so on

--- react-data-fetching_iss-tracker/pages/_app.js
+++ react-data-fetching_iss-tracker/pages/_app.js
@@ -1,10 +1,20 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+import { Fetcher } from "swr";
+import { fetcher } from "../components/ISSTracker";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig
+        value={{
+          fetcher,
+          refreshInterval: 1000,
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
